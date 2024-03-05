67740d058aa9f40fa293aaf09853d0caab6d9cf3
Shadkoech <shadrackipruto@gmail.com>
2024-03-05 04:57:11 +0300

    For of ES6 method to iterate over an array

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newarray = [];
+
+  for (const idx of array) {
+    const value = idx;
+    newarray.push(appendString + value);
+  }
+
+  return newarray;
+}
