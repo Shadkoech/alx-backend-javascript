799c1fd3c8f46192f19ea02cba9d8ec9edd01b51
Shadkoech <shadrackipruto@gmail.com>
2024-03-06 12:33:48 +0300

    Unfulfilled promise
    .then fulfilled the promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,14 @@
-export default function getFullResponseFromAPI(success) {
+function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({
         status: 200,
-        body: 'success',
+        body: 'Success',
       });
+    } else {
+      reject(new Error('The fake API is not working currently'));
     }
-    reject(new Error('The fake API is not working currently'));
   });
 }
+
+export default getFullResponseFromAPI;
