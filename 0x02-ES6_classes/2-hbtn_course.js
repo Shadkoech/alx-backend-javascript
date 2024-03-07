b0296d4e01dd82e5d70499805e8ac5847eabb8a8
Shadkoech <shadrackipruto@gmail.com>
2024-03-07 16:39:10 +0300

    Getters and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,35 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('name must be a string');
+    if (typeof length !== 'number') throw new TypeError('length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('Students must be an array of strings');
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    return this._students = students;
+  }
+}
