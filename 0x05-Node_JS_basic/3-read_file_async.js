010e86551d54339390dc8e1747f92f5a7266997b
Shadkoech <shadrackipruto@gmail.com>
2024-04-30 12:59:14 +0300

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,47 @@
+// Import 'fs' module for handling the file system
+const fs = require('fs');
+
+// Define function taking path argument that specify path to database file
+async function countStudents(path) {
+  if (fs.existsSync(path)) {
+    return new Promise((resolve) => {
+      // Read the database file asynchronously
+      fs.readFile(path, 'utf8', (error, data) => {
+        if (error) {
+          throw Error('Cannot load the database');
+        }
+
+        // Parse the data and split it using '\n' and ',' as delimeters Store the result in an array
+        const result = [];
+        data.split('\n').forEach((data) => {
+          result.push(data.split(','));
+        });
+
+        // Extract and filter needed info and add 1st and 4th fields to array newFields
+        result.shift();
+        const newFields = [];
+        result.forEach((data) => {
+          newFields.push([data[0], data[3]]);
+        });
+
+        // Count, aggregate the data and create a new set
+        const fields = new Set();
+        newFields.forEach((item) => {
+          fields.add(item[1]);
+        });
+        const final = {};
+        fields.forEach((data) => { (final[data] = 0); });
+        newFields.forEach((data) => { (final[data[1]] += 1); });
+        // Display the results on the standard output
+        console.log(`Number of students: ${result.filter((check) => check.length > 3).length}`);
+        Object.keys(final).forEach((data) => console.log(`Number of students in ${data}: ${final[data]}. List: ${newFields.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}`));
+
+        // Handle the error if the database is not present.
+        resolve(result, final, newFields);
+      });
+    });
+  }
+  throw Error('Cannot load the database');
+}
+
+module.exports = countStudents;
