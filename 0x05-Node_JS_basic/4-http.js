bb1f30e261ea2cbdb69035399fe20b9315bd84ee
Shadkoech <shadrackipruto@gmail.com>
2024-04-30 16:17:56 +0300

    Simple http server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+// destructuring assignment
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+// Creating the HTTP server
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {});
+
+module.exports = app;
